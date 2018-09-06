c2e728d075e83ba29cbe8da2cf74d476fcaa6b81
angjreyes <2015086566@ust-ics.mygbiz.com>
2018-09-06 20:07:23 +0800

    Initial commit

---
+++ app/src/main/java/angelapamela/reyes/com/reyesangelapamelalab2/MainActivity.java
@@ -0,0 +1,13 @@
+package angelapamela.reyes.com.reyesangelapamelalab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint_layout);
+    }
+}
